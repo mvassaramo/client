d8fbb0dedf8440460be0a0280abd75664bcfd19f
Maduri Vassaramo <maduri@vinterior.co>
2021-08-06 17:54:46 +0200

    add label and styling to StreamCreate ui

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -3,17 +3,23 @@ import { Field, reduxForm } from 'redux-form'
 
 class StreamCreate extends React.Component {
 
-  renderInput({ input }) {
-    return <input {...input} />
+  renderInput({ input, label }) {
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+
+      </div>
+    )
   }
 
   render() {
     return(
       <div>
         <h1>StreamCreate</h1>
-        <form>
-          <Field name="title" component={this.renderInput}/>
-          <Field name="description" component={this.renderInput} />
+        <form className="ui form">
+          <Field name="title" component={this.renderInput} label="Enter title" />
+          <Field name="description" component={this.renderInput} label="Enter description" />
 
         </form>
       </div>
