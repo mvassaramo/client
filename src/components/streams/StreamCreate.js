b19ba447d33afcb61549e77593d1a303b86797e0
Maduri Vassaramo <maduri@vinterior.co>
2021-08-06 17:50:40 +0200

    add redux fields components to StreamCreate component

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,34 @@
 import React from 'react'
+import { Field, reduxForm } from 'redux-form'
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>
+class StreamCreate extends React.Component {
+
+  renderInput({ input }) {
+    return <input {...input} />
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>StreamCreate</h1>
+        <form>
+          <Field name="title" component={this.renderInput}/>
+          <Field name="description" component={this.renderInput} />
+
+        </form>
+      </div>
+
+    )
+  }
 }
 
-export default StreamCreate
+export default reduxForm({
+  form: 'streamCreate'
+})(StreamCreate)
+
+
+
+// Field component always has to be given name prop
+// Field is responsible for redux to handle the data but not what gets
+// rendered on the screen, which is why component prop needs to be passed down
+// component prop will either be a react component or a function for it to call
