546e17f43d7470a4bf7579eb3a745dd705d5eea8
Maduri Vassaramo <maduri@vinterior.co>
2021-09-07 16:17:33 +0200

    add userID to createStream

--- src/actions/index.js
+++ src/actions/index.js
@@ -28,8 +28,9 @@ export const signOut = () => {
 
 // steam actions
 
-export const createStream = formValues => async dispatch => {
-  const response = await streams.post('/streams', formValues)
+export const createStream = formValues => async (dispatch, getState) => {
+  const { userId } = getState().auth
+  const response = await streams.post('/streams', { ...formValues, userId })
 
   dispatch({ type: CREATE_STREAM, payload: response.data })
 }
