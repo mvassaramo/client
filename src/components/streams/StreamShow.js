ee1dca7723f5aada66596d91c70d7e3ed0bd09e7
Maduri Vassaramo <maduri@vinterior.co>
2021-10-14 09:38:32 +0200

    update streamShow component to stream videos using flv

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,10 +1,42 @@
 import React from 'react'
+import flv from 'flv.js'
 import { connect } from 'react-redux'
 import { fetchStream } from '../../actions'
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props)
+    this.videoRef = React.createRef()
+  }
+
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id)
+    this.buildPlayer()
+  }
+
+  componentDidUpdate() {
+    // if component fetches stream at some point in the future
+    // and not on initial render, need to build player
+    this.buildPlayer()
+  }
+
+  componentWillUnmount() {
+    this.player.destroy()
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return
+    }
+
+    const { id } = this.props.match.params
+
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`
+    })
+    this.player.attachMediaElement(this.videoRef.current)
+    this.player.load()
   }
 
   render() {
@@ -16,6 +48,10 @@ class StreamShow extends React.Component {
 
     return(
       <div>
+        <video
+          style={{ width: '100%' }}
+          controls //boolean so same as controls={true}
+          ref={this.videoRef}/>
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
